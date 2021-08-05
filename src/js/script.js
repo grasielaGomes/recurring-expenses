c73dbb21b4dbe536ddbe81ba6c4ee4b95e8dafc6
Grasiela Gomes <produtoradama@gmail.com>
2021-08-05 18:48:49 -0300

    modal js

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const Modal = {
+  open(){
+    document.querySelector('.modal-overlay').classList.add('active');
+  },
+  close(){
+    document.querySelector('.modal-overlay').classList.remove('active');
+  }
+}
