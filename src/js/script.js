19c81f8f49197244cd2359b88dd7ac87e4aa2464
Grasiela Gomes <produtoradama@gmail.com>
2021-08-06 20:50:24 -0300

    transactions js

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,51 @@
 const Modal = {
-  open(){
-    document.querySelector('.modal-overlay').classList.add('active');
+  activate(){
+    document.querySelector('.modal-overlay').classList.toggle('active');
+  }
+}
+
+const transactions = [
+  {
+    id: 1,
+    description: 'Internet',
+    amount: 50000,
+    date: '23/08/2021'
   },
-  close(){
-    document.querySelector('.modal-overlay').classList.remove('active');
+  {
+    id: 2,
+    description: 'Netflix',
+    amount: 4000,
+    date: '23/08/2021'
+  }
+];
+
+const Transaction = {
+  incomes() {
+    //pegar as entradas
+  },
+  expenses() {
+    //somar as saidas
+  },
+  total() {
+    //entradas - saidas
+  }
+}
+
+const htmlObj = {
+  transactionsContainer: document.querySelector('.data-table tbody'),
+  addTransaction(transaction, index) {
+    const tr = document.createElement('tr');
+    tr.innerHTML = this.innerHTMLTransaction(transaction);
+    this.transactionsContainer.appendChild(tr);
+  },
+  innerHTMLTransaction(transaction) {
+    return `
+        <td class="description">${transaction.description}</td>
+        <td class="expense">${transaction.amount}</td>
+        <td class="date">${transaction.date}</td>
+        <td>
+          <img src="./src/images/minus.svg" alt="Remover despesa">
+        </td>
+    `
   }
 }
