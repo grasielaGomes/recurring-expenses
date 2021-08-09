5a9b4b3b2e63bc36d7e512c9678de940fa7c257e
Grasiela Gomes <produtoradama@gmail.com>
2021-08-09 14:12:24 -0300

    get incomes and expenses

--- src/js/script.js
+++ src/js/script.js
@@ -6,19 +6,7 @@ const Modal = {
 
 const Transaction = {
   all: [
-    {
-      description: 'Internet',
-      amount: 50000,
-      date: '23/08/2021'
-    },
-    {
-      description: 'Netflix',
-      amount: 4000,
-      date: '23/08/2021'
-    }
   ],
-  income: document
-    .getElementById('income-display'),
   add(transaction) {
     this.all.push(transaction);
     Add.reload();
@@ -27,55 +15,41 @@ const Transaction = {
     this.all.splice(index, 1);
     App.reload();
   },
+  income() {
+    return document
+    .getElementById('income-display').value; 
+  },
   expenses() {
     return this.all
       .reduce((acc, { amount }) => acc + amount, 0);
   },
   total() {
-    return this.income.value - this.expenses();
-  }
-}
-
-const Utils = {
-  formatCurrency (value) {
-    let signal = "";
-    if (value < 0) signal = "-";
-    value = String(value).replace(/\D/g, "");
-    value = Number(value) / 100;
-    value = value.toLocaleString("pt-BR", {
-      style: "currency",
-      currency: "BRL"
-    })
-    return signal + value;
-  },
-  changeBgColor (element, condition) {
-    if (condition) element.style.backgroundColor = 'var(--custom-green)';
-    else element.style.backgroundColor = 'var(--custom-pink)';
+    return Utils.formatAmount(this.income()) - this.expenses();
   }
 }
 
 const htmlCards = {
   cardTotal: document.querySelector('.total'),
+  income: document.getElementById('income-display'),
   expense: document.getElementById('expense-display'),
   total: document.getElementById('total-display'),
-  changeValueVisibility () {
-    if (Transaction.all.length === 0) {
-      this.expense.classList.toggle('hidden');
-      this.total.classList.toggle('hidden');
-    }
+  changeValueVisibility() {
+    this.expense.classList.remove('hidden');
+    this.total.classList.remove('hidden');
   },
-  updateBalance () {
+  updateBalance() {
     this.expense.innerText = Utils.formatCurrency(Transaction.expenses());
     this.total.innerText = Utils.formatCurrency(Transaction.total());
     Utils.changeBgColor(this.cardTotal, Transaction.total() > 0);
   },
-  getIncomeValue() {
-    Transaction.income.addEventListener('blur', function (e) {
-      let digits = e.target.value.replace(/\D/g, "");
+  updateIncomeValue() {
+    this.income.addEventListener('blur', function (e) {
+      let digits = Utils.replaceToDigits(e.target.value);
       let total = parseInt(digits) - Transaction.expenses();
-      Transaction.income.value = Utils.formatCurrency(e.target.value);
+      htmlCards.income.value = Utils.formatCurrency(e.target.value);
       document.getElementById('total-display').innerHTML = Utils.formatCurrency(total);
       Utils.changeBgColor(htmlCards.cardTotal, total >= 0);
+      htmlCards.changeValueVisibility();
     });
   }
 }
@@ -107,21 +81,55 @@ const Form = {
   description: document.querySelector('input#expense-description'),
   amount: document.querySelector('input#expense-amount'),
   date: document.querySelector('input#expense-date'),
-  validateFields(){},
-  formatData(){},
+  getValues(){
+    return {
+      description: this.description.value,
+      amount: this.amount.value,
+      date: this.date.value
+    }
+  },
+  clearFields(){
+    this.description.value = '';
+    this.amount.value = '';
+    this.date.value = '';
+  },
+  validateFields(){
+    const { description, amount, date } = this.getValues();
+    if(description.trim() === "" || amount.trim() === "" || date.trim() === "") throw new Error("Por favor, preencha todos os campos.");
+  },
+  formatValues(){
+    let { description, amount, date } = this.getValues();
+    amount = Utils.formatAmount(amount);
+    date = Utils.formatDate(date);
+    return {
+      description,
+      amount,
+      date
+    }
+  },
   submit(event){
-    event.preventDefault();
-    this.validateFields();
-    this.formatData();
+    try {
+      event.preventDefault();
+      this.validateFields();
+      Transaction.add(this.formatValues());
+      htmlCards.changeValueVisibility();
+      this.clearFields();
+      Modal.activate();
+      App.reload();
+    } catch(e) {
+      const errorMessage = document.querySelector('#form-error');
+      errorMessage.innerText = e.message;
+      errorMessage.classList.remove('hidden');
+      window.setTimeout(() => errorMessage.classList.add('hidden'), 2000);
+    }
   }
 }
 
 const Add = {
   init(){
     Transaction.all.forEach(t => htmlTableElement.addTransaction(t));
+    htmlCards.updateIncomeValue();
     htmlCards.updateBalance();
-    htmlCards.changeValueVisibility();
-    htmlCards.getIncomeValue();
   },
   reload(){
     htmlTableElement.clearTransactions();
@@ -129,13 +137,38 @@ const Add = {
   }
 }
 
+const Utils = {
+  formatCurrency(value) {
+    let signal = "";
+    if (value < 0) signal = "-";
+    value = String(value).replace(/\D/g, "");
+    value = Number(value) / 100;
+    value = value.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL"
+    })
+    return signal + value;
+  },
+  formatAmount(value) {
+    value = this.replaceToDigits(value);
+    return Number(value);
+  },
+  formatDate(value){
+    const splittedDate = value.split("-");
+    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`;
+  },
+  changeBgColor (element, condition) {
+    if (condition) element.style.backgroundColor = 'var(--custom-green)';
+    else element.style.backgroundColor = 'var(--custom-pink)';
+  },
+  replaceToDigits(value){
+    return value.replace(/\D/g, "");
+  }
+}
+
+
 Add.init();
 
-Transaction.add({
-  description: 'TV a cabo',
-  amount: 53000,
-  date: '23/08/2021'
-});
 
 
 
